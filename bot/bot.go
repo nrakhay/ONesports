bbf1c9894210e61cc36c20c3f94f0427b36a42c8
Nurali <nuralirakhay@gmail.com>
2024-05-09 02:10:18 +0500

    [add]: voice channel creation handler

--- bot/bot.go
+++ bot/bot.go
@@ -3,6 +3,10 @@ package bot
 import (
 	"fmt"
 
+	"log"
+
+	"time"
+
 	"github.com/nrakhay/ONEsports/config"
 
 	"github.com/bwmarrin/discordgo"
@@ -19,15 +23,17 @@ func Start() {
 		return
 	}
 
-	u, err := goBot.User("@me")
+	user, err := goBot.User("@me")
 
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	BotId = u.ID
+	BotId = user.ID
 
 	goBot.AddHandler(messageHandler)
+
+	goBot.AddHandler(channelCreateHandler)
 
 	err = goBot.Open()
 
@@ -39,16 +45,42 @@ func Start() {
 	fmt.Println("Bot is running!")
 }
 
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == BotId {
+func channelCreateHandler(session *discordgo.Session, channel *discordgo.ChannelCreate) {
+	if channel.Type == discordgo.ChannelTypeGuildVoice {
+		log.Printf("A new voice channel was created: %s", channel.Name)
+
+		// Join the voice channel
+		vc, err := session.ChannelVoiceJoin(channel.GuildID, channel.ID, false, true)
+		if err != nil {
+			log.Printf("Failed to join the voice channel: %s", err)
+			return
+		}
+
+		
+		log.Println("Joined the new voice channel:", channel.Name)
+
+		// TODO: Start capturing audio from the voice channel
+
+		// Disconnect after 10 seconds
+		go func() {
+			select {
+			case <-time.After(10 * time.Second):
+				vc.Disconnect()
+			}
+		}()
+	}
+}
+
+func messageHandler(session *discordgo.Session, message *discordgo.MessageCreate) {
+	if message.Author.ID == BotId {
 		return
 	}
 
-	if m.Content == "<@"+BotId+"> !ping" || m.Content == "<@"+BotId+"> ping" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "pong!")
+	if message.Content == "<@"+BotId+"> !ping" || message.Content == "<@"+BotId+"> ping" {
+		_, _ = session.ChannelMessageSend(message.ChannelID, "pong!")
 	}
 
-	if m.Content == config.BotPrefix+"ping" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "pong!")
+	if message.Content == config.BotPrefix+"ping" {
+		_, _ = session.ChannelMessageSend(message.ChannelID, "pong!")
 	}
 }
