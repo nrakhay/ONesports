ad4e7ddcbd6d2d85bf94c1e95c242ff9dd9c9812
Nurali <nuralirakhay@gmail.com>
2024-05-09 20:54:35 +0600

    [add]: basic vc listener logic

--- bot/bot.go
+++ bot/bot.go
@@ -2,14 +2,14 @@ package bot
 
 import (
 	"fmt"
-
 	"log"
-
 	"time"
 
-	"github.com/nrakhay/ONEsports/config"
-
 	"github.com/bwmarrin/discordgo"
+	"github.com/nrakhay/ONEsports/config"
+	"github.com/pion/rtp"
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
 )
 
 var BotId string
@@ -17,26 +17,20 @@ var goBot *discordgo.Session
 
 func Start() {
 	goBot, err := discordgo.New("Bot " + config.Token)
-
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
 	user, err := goBot.User("@me")
-
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	BotId = user.ID
-
 	goBot.AddHandler(messageHandler)
-
 	goBot.AddHandler(channelCreateHandler)
-
 	err = goBot.Open()
-
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -45,29 +39,67 @@ func Start() {
 	fmt.Println("Bot is running!")
 }
 
-func channelCreateHandler(session *discordgo.Session, channel *discordgo.ChannelCreate) {
-	if channel.Type == discordgo.ChannelTypeGuildVoice {
-		log.Printf("A new voice channel was created: %s", channel.Name)
-
-		// Join the voice channel
-		vc, err := session.ChannelVoiceJoin(channel.GuildID, channel.ID, false, true)
+func channelCreateHandler(s *discordgo.Session, c *discordgo.ChannelCreate) {
+	if c.Type == discordgo.ChannelTypeGuildVoice {
+		log.Printf("A new voice channel was created: %s", c.Name)
+		vc, err := s.ChannelVoiceJoin(c.GuildID, c.ID, false, false)
 		if err != nil {
 			log.Printf("Failed to join the voice channel: %s", err)
 			return
 		}
 
-		
-		log.Println("Joined the new voice channel:", channel.Name)
+		log.Println("Joined the new voice channel:", c.Name)
+		go handleVoice(vc.OpusRecv)
 
-		// TODO: Start capturing audio from the voice channel
-
-		// Disconnect after 10 seconds
 		go func() {
 			select {
 			case <-time.After(10 * time.Second):
 				vc.Disconnect()
 			}
 		}()
+	}
+}
+
+func createPionRTPPacket(p *discordgo.Packet) *rtp.Packet {
+	return &rtp.Packet{
+		Header: rtp.Header{
+			Version:        2,
+			PayloadType:    0x78,
+			SequenceNumber: p.Sequence,
+			Timestamp:      p.Timestamp,
+			SSRC:           p.SSRC,
+		},
+		Payload: p.Opus,
+	}
+}
+
+func handleVoice(c chan *discordgo.Packet) {
+	files := make(map[uint32]media.Writer)
+	defer func() {
+		for _, f := range files {
+			f.Close()
+		}
+	}()
+
+	for p := range c {
+		log.Printf("Received packet from SSRC: %d", p.SSRC)
+		file, ok := files[p.SSRC]
+		if !ok {
+			var err error
+			file, err = oggwriter.New(fmt.Sprintf("%d.ogg", p.SSRC), 48000, 2)
+			if err != nil {
+				log.Printf("Failed to create file %d.ogg, giving up on recording: %v", p.SSRC, err)
+				return
+			}
+			files[p.SSRC] = file
+			log.Printf("Created new file for SSRC: %d", p.SSRC)
+		}
+
+		rtp := createPionRTPPacket(p)
+		err := file.WriteRTP(rtp)
+		if err != nil {
+			log.Printf("Failed to write to file %d.ogg, giving up on recording: %v", p.SSRC, err)
+		}
 	}
 }
 
--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/bwmarrin/discordgo v0.28.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtp v1.8.6 // indirect
+	github.com/pion/webrtc/v3 v3.2.40 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
